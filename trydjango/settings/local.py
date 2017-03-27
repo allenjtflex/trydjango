153200d8900fa6c8bc0e51a8fc45ab306923eda7
allen.chang <allen.chang@localhost.localdomain>
2017-03-27 20:22:38 +0800

    set static folder

--- trydjango/settings/local.py
+++ trydjango/settings/local.py
@@ -119,3 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT =  os.path.join(os.path.dirname(BASE_DIR), "trydjango/static", "media" )
+STATIC_ROOT =  os.path.join(os.path.dirname(BASE_DIR), "trydjango/static", "static_root" )
+STATICFILES_DIRS = (
+          os.path.join(os.path.dirname(BASE_DIR), "trydjango/static", "static_files" ),
+    )
